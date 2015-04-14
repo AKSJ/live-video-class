059b2c5a3b8c8fea1436cd5db56902254f90bcb6
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-04-14 18:50:55 +0100

    working start/stop stream buttons. NB - publisher left in place, you can still see yourself, ut other clients cannot (tested)

--- api/models/members.js
+++ api/models/members.js
@@ -6,7 +6,7 @@ var Member		= schema.Member;
 // 		optional ----->	filter: {'permissions': 1, 'username':1, _id': 0}
 // 					}
 exports.search = function(params, onComplete) {
-
+	// NB - Probably want find rather than findOne for general search
 	if (params.filter) {
 		Member.findOne(params.query, params.filter, function(err, result){
 			if (err) {
--- public/scripts/test.js
+++ public/scripts/test.js
@@ -1,3 +1,6 @@
+// NB - Currently (sometimes) getting a dev console error 'cannnot read property 'videoWidth of null' on unpublish
+// This may be a bug, code seems okay as far as I can tell. See: http://webcache.googleusercontent.com/search?q=cache:EEXBFdO8mQsJ:https://forums.tokbox.com/bugs/cannot-read-property-videowidth-of-null-error-t45250+&cd=1&hl=en&ct=clnk&gl=uk
+
 // Initialize an OpenTok Session object
 var session = TB.initSession(sessionId);
 
@@ -15,11 +18,9 @@ session.on({
 
 	// This function runs when session.connect() asynchronously completes
 	sessionConnected: function(event) {
-
-		// Initialize a Publisher, and place it into the element with id="publisher"
-		var publisher = TB.initPublisher(apiKey, 'publisher');
 		// Publish the publisher (this will trigger 'streamCreated' on other
 		// clients)
+		console.log(event);
 		session.publish(publisher);
 	},
 
@@ -40,29 +41,54 @@ session.on({
 		session.subscribe(event.stream, subContainer);
 	},
 
-	streamDestoyed: function(event) {
+	streamDestroyed: function(event) {
 		//Check if stream is currently displayed, if so remove from DOM and adjust count/activeStreams
 		// Not currently unsubscribing, as defualt behaviour should handle that.
+		var subscribers = session.getSubscribersForStream(event.stream);
+		console.log(subscribers);
 		var streamId = event.stream.streamId;
 		var streamIndex = activeStreams.indexOf(streamId);
+		console.log('streamId: '+streamId, 'streamIndex: '+streamIndex);
 		if (streamIndex !== -1) {
-			$('#stream-'+streamId).remove(function(){
-				streamCount--;
-				activeStreams.splice(streamIndex,1);
-			});
+			$('#stream-'+streamId).remove();
+			streamCount--;
+			activeStreams.splice(streamIndex,1);
+			console.log(streamCount, activeStreams);
 		}
 	}
 
 });
 
+publisher.on({
+	streamDestroyed: function(event) {
+		// Check if stream is our own. We want to leave it in place if so.
+		console.log('Publisher Event:');
+		console.log(event);
+		var subscribers = session.getSubscribersForStream(event.stream);
+		console.log(subscribers);
+		if (event.stream.connection.connectionId === session.connection.connectionId) {
+			console.log('ConnectionId match');
+			event.preventDefault();
+		}
+	}
+});
+
 // Connect to the Session using the 'apiKey' of the application and a 'token' for permission
 session.connect(apiKey, token);
 
+
+// NB - unpublish is currently working, you still see yourself locally, but other clients don't (tested over network)
+// Thought that would be better than losing the local video/publisher object
+// ??? - We could just switch off audio and video. Better or worse...?
 $('#stopStream').click(function(){
 	session.unpublish(publisher);
+	// publisher.publishVideo(false);
+	// publisher.publishAudio(false);
 });
 
 $('#startStream').click(function(){
 	session.publish(publisher);
+	// publisher.publishVideo(true);
+	// publisher.publishAudio(true);
 });
 
