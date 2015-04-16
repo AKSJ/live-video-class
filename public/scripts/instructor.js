1046db6f3c5e78527c52a4261458d012fe5d51ed
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-04-16 14:05:53 +0100

    new subscribers now find empty containers by jquery magic

--- public/scripts/instructor.js
+++ public/scripts/instructor.js
@@ -9,10 +9,8 @@ console.log("Username: " + username );
 console.log("Permissions: " + permissions );
 
 // Initialize a Publisher, and place it into the element with id="publisher"
-var publisher = OT.initPublisher( /*apiKey,*/ 'publisher', {"name": username, width: 400, height: 300, style: {nameDisplayMode: "on"}});
+var publisher = OT.initPublisher( /*apiKey,*/ 'publisher', {"name": username, width: '33.33%', height: '50%', style: {nameDisplayMode: "on"}});
 
-// TODO?: remove streamCount and use activeStreams.length? or subscribers.keys.length?
-var streamCount = 0;
 var activeStreams = [];
 var inactiveStreams = [];
 var subscribers = {};
@@ -38,12 +36,12 @@ session.on({
 		console.log(event);
 		var stream = event.stream;
 		var streamId = event.stream.streamId;
-		if (streamCount < 5) {
-			streamCount++;
+		var activeStreamCount = activeStreams.length;
+		if (activeStreamCount < 5) {
 			activeStreams.push(stream);
-			subContainerId = 'stream-' + streamId;
-			$('<div/>').attr('id',subContainerId).appendTo('#subscriber' + streamCount);
-			subscribers[streamId] = session.subscribe(event.stream, subContainerId, {width: 400, height: 300});
+			var subContainerId = 'stream-' + streamId;
+			$('<div/>').attr('id',subContainerId).appendTo($('.subscriber:empty'));
+			subscribers[streamId] = session.subscribe(event.stream, subContainerId, {width: 400, height: 300, style: {nameDisplayMode: 'on'}});
 		}
 		else {
 			inactiveStreams.push(stream);
@@ -70,33 +68,18 @@ session.on({
 
 		if (activeStreamIndex !== null) {
 			console.log('destroyedStreamId: '+destroyedStreamId, 'activeStreamIndex: '+activeStreamIndex);
-			$('#stream-'+destroyedStreamId).remove();
-			streamCount--;
+			// $('#stream-'+destroyedStreamId).remove(); // default behaviour should remove the DOM element?
 			activeStreams.splice(activeStreamIndex,1);
-			console.log('Stream count should === activeStreams.length ',streamCount, activeStreams.length);
 		}
 		if (inactiveStreamIndex !== null) {
 			inactiveStreams.splice(inactiveStreamIndex,1);
 		}
 		if (subscribers[destroyedStreamId]) {
 			delete subscribers[destroyedStreamId];
-			console.log('subscribers should have same num props as stream count, streamCount: ', streamCount, 'Subscribers length: '+ subscribers.keys.length);
+			console.log('subscribers should have same num props as activeStream.length, activeStreams.length: ', activeStreams.length, 'Subscribers length: '+ Object.keys(subscribers).length);
 		}
 	}
 });
-
-// if < 5 active streams, check for inactive streams and subscribe
-setInterval(function(){
-	if (streamCount < 5 && inactiveStreams.length > 0) {
-		var newStream = inactiveStreams.pop();
-		var newStreamId = newStream.streamId;
-		streamCount++;
-		activeStreams.push(newStream);
-		subContainerId = 'stream-' + newStreamId;
-		$('<div/>').attr('id',subContainerId).appendTo('#subscriber' + streamCount);
-		subscribers[newStreamId] = session.subscribe(newStream, subContainerId, {width: 400, height: 300});
-	}
-},500);
 
 publisher.on({
 	streamDestroyed: function(event) {
@@ -110,9 +93,23 @@ publisher.on({
 	}
 });
 
-// Connect to the Session using the 'apiKey' of the application and a 'token' for permission
 session.connect(/*apiKey,*/ token);
 
+// if < 5 active streams, check for inactive streams and subscribe
+setInterval(function(){
+	var activeStreamCount = activeStreams.length;
+	var streamsToAddCount = 5 - activeStreamCount;
+	if (activeStreamCount < 5 && inactiveStreams.length > 0) {
+		for (var i=0; i<streamsToAddCount; i++) {
+			var newStream = inactiveStreams.pop();
+			var newStreamId = newStream.streamId;
+			activeStreams.push(newStream);
+			var subContainerId = 'stream-' + newStreamId;
+			$('<div/>').attr('id',subContainerId).appendTo($('.subscriber:empty'));
+			subscribers[newStreamId] = session.subscribe(newStream, subContainerId, {width: 400, height: 300, style: {nameDisplayMode: 'on'}});
+		}
+	}
+},500);
 
 // NB - unpublish is currently working, you still see yourself locally, but other clients don't (tested over network)
 // Thought that would be better than losing the local video/publisher object
@@ -144,24 +141,34 @@ $('#nextFive').click(function(){
 			var tempStream = inactiveStreams.pop();
 			tempStreams.push(tempStream);
 		}
-		// 1. Clear deactivated subscribers. Move deactiavted streams to inactive and unsubscribe
+		// 1. Clear deactivated subscribers, and unsubscribe
 		tempStreams.forEach(function(tempStream, index){
 			// unshft should remove subscriber1 in DOM first, gives impression of scrolling in <5 deactivated
 			var streamToKill = activeStreams.unshift();
 			var streamToKillId = streamToKill.streamId;
 			if (subscribers[streamToKillId]) {
-				session.unsubscribe(subscribers[streamToKillId]); // unsubscribe takes a subscriber object. Knew i kept them for a reason :D
+				session.unsubscribe(subscribers[streamToKillId]); // unsubscribe takes a subscriber object. Knew I kept them for a reason :D
 				delete subscribers[streamToKillId];
 			}
-
+			$('#stream-'+ streamToKillId).remove();
+		// 2. Get up to 5 inactive streams, make active, subscribe
+		});
+		tempStreams.forEach(function(tempStream, index){
+			var streamToActivate = inactiveStreams.pop();
+			activeStreams.push(streamToActivate);
+			var subContainerId = 'stream-' + streamId;
+			$('<div/>').attr('id',subContainerId).appendTo($('.subscriber:empty'));
+			subscribers[streamId] = session.subscribe(event.stream, subContainerId, {width: 400, height: 300,  style: {nameDisplayMode: 'on'}});
+		});
+		tempStreams.forEach(function(tempStream, index){
+			inactiveStreams.push(tempStream);
 		});
 	}
 
-
-	// 2. Get up to 5 inactive streams, make active, subscribe
 });
 
 $('#getSubscribers').click(function(){
+	console.log(subscribers);
 	if (activeStreams) {
 		activeStreams.forEach(function(stream, index){
 			console.log(session.getSubscribersForStream(stream));
