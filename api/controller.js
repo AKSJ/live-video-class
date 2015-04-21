f4b30420c9f2259f8991856649e1f0b97b16377a
sarah <sarahabimay@gmail.com>
2015-04-21 11:15:55 +0100

    remove comment and gif

--- api/controller.js
+++ api/controller.js
@@ -143,7 +143,6 @@ module.exports = {
 					console.log( "Username: " + username );
 					console.log( "Permissions: " + userPermissions);
 					console.log( "TokBox Role: " + permissionsList[ userPermissions]);
-					// if( permissionsList[ userPermissions ] === undefined ){
 					if ( !permissionsList.hasOwnProperty(userPermissions) ) {
 						return reply.view('invalidUser', { error: "You do not have valid permissions" });
 					}
